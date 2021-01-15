9b19e46b7b58b80fadeff287775258d0853b8ca9
marvinm2 <marvin.martens@maastrichtuniversity.nl>
2021-01-15 12:03:09 +0100

    updated from WP

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+
+(function ($) {
+
+jQuery(document).ready(function() {
+
+        var cookieDecision = getCookie('cookieDecision');
+
+        if (cookieDecision == "" || cookieDecision == "reject") {
+
+            $('#cookieModal').modal();
+
+            $('#cookieModal button.btn-secondary').on('click', function (e) {
+                setCookie("cookieDecision", "reject");
+                window.location.href = "https://aopwiki.org";
+            });
+
+            $("#cookieModal button.btn-primary").on('click', function(){
+                setCookie("cookieDecision", "accept");
+                $('#cookieModal').modal('hide');
+            });
+        }
+
+        jQuery("#query-button").on("click",function(event){
+            event.preventDefault();
+
+            var query = editor.getDoc().getValue();
+            var queryText = getPrefixes() + query;
+            doQuery(jQuery("#endpoint").val(), query, function(json) { displayResult(json, "SPARQL results"); });
+
+		});
+
+		jQuery("#fetch").on("click",function(){
+            fetchExamples();
+		});
+
+        //---------------- Search funcionality starts ------------------------
+
+        var search = function(e) {
+          var pattern = $('#input-search').val();
+          var options = {
+            ignoreCase: true,
+            exactMatch: false,
+            revealResults: true
+          };
+          var results = $('#examples').treeview('search', [ pattern, options ]);
+        }
+
+        $('#btn-search').on('click', search);
+
+        $('#btn-clear-search').on('click', function (e) {
+          $('#examples').treeview('clearSearch');
+          $('#input-search').val('');
+          $('#search-output').html('');
+        });
+
+        //---------------- Search funcionality ends ------------------------
+
+        jQuery("#reset-button").on("click",function(){
+            editor.getDoc().setValue("");
+        });
+
+        jQuery("#export-csv").on("click",function(){
+            var query = editor.getDoc().getValue();
+            var queryText = getPrefixes() + query;
+            exportResults(jQuery("#endpoint").val(), query, "csv");
+        });
+
+        jQuery("#export-json").on("click",function(){
+            var query = editor.getDoc().getValue();
+            var queryText = getPrefixes() + query;
+            exportResults(jQuery("#endpoint").val(), query, "json");
+        });
+
+        jQuery("#export-xml").on("click",function(){
+            var query = editor.getDoc().getValue();
+            var queryText = getPrefixes() + query;
+            exportResults(jQuery("#endpoint").val(), query, "xml");
+        });
+} );
+})(jQuery);
